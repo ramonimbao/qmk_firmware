5e68794fcaf521225973fe4cca7cd2251bcc0031
Ramon Imbao <ramonimbao@gmail.com>
2022-06-13 22:59:25 +0800

    Remove trilayer state

--- keyboards/mc40/keymaps/default/keymap.c
+++ keyboards/mc40/keymaps/default/keymap.c
@@ -30,10 +30,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, 1, 2, 3);
-}
-
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
         tap_code_delay(KC_VOLU, 10);
--- keyboards/mc40/keymaps/via/keymap.c
+++ keyboards/mc40/keymaps/via/keymap.c
@@ -30,10 +30,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, 1, 2, 3);
-}
-
 keyevent_t encoder_ccw = {
     .key = (keypos_t){.row = 4, .col = 0},
     .pressed = false
