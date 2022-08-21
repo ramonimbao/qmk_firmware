ed8d815b60c2684d6400f935b6242197265900b7
Ramon Imbao <ramonimbao@gmail.com>
2022-08-21 14:08:14 +0800

    Add Oddball Plus

---
+++ keyboards/oddballplus/config.h
@@ -0,0 +1,55 @@
+/* Copyright 2020 Alexander Tulloh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+#define MATRIX_ROW_PINS { D4, E6, D7, C6, F4 }
+#define MATRIX_COL_PINS { B6, B5, B4, F5, F6, F7 }
+
+/* key matrix size */
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 6
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+/* Split Keyboard specific options */
+#define USE_I2C
+#define SPLIT_USB_DETECT
+#define MASTER_RIGHT
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* optical sensor settings */
+#define SCROLL_DIVIDER 12
+#define CPI_1 2000
+#define CPI_2 4000
+#define CPI_3 8000
+
+/* Bootmagic Lite key configuration */
+// #define BOOTMAGIC_LITE_ROW 0
+// #define BOOTMAGIC_LITE_COLUMN 0
+
+#define ADNS9800_CS_PIN SPI_SS_PIN
+#define PMW3360_CS_PIN SPI_SS_PIN
