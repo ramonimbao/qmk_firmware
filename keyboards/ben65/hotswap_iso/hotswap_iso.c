debcf6f7dd49b483e2c26cbc0f8e3c7c201e00e1
Ramon Imbao <ramonimbao@gmail.com>
2022-09-07 23:51:31 +0800

    Add Ben65 keyboards

---
+++ keyboards/ben65/hotswap_iso/hotswap_iso.c
@@ -0,0 +1,40 @@
+/* Copyright 2022 Ramon Imbao
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "hotswap_iso.h"
+
+led_config_t g_led_config = {{
+    // Key Matrix to LED Index
+    {      0,      1,      2,      3,      4,      5,      6,      7,      8,      9,     10,     11,     12,     13, NO_LED },
+    {     29,     28,     27,     26,     25,     24,     23,     22,     21,     20,     19,     18,     17,     16,     14 },
+    {     30,     31,     32,     33,     34,     35,     36,     37,     38,     39,     40,     41,     NO_LED, 42,     15 },
+    {     56,     NO_LED, 55,     54,     53,     52,     51,     50,     49,     48,     47,     46,     45,     44,     43 },
+    {     57,     58,     59, NO_LED, NO_LED, NO_LED,     60, NO_LED, NO_LED,     61,     62,     63,     64,     65, NO_LED }
+}, {
+    // LED Index to Position
+    {  0,0  }, { 14,0  }, { 29,0  }, { 43,0  }, { 58,0  }, { 72,0  }, { 87,0  }, {101,0  }, {116,0  }, {130,0  }, {145,0  }, {159,0  }, {173,0  }, {195,0  }, {224,0  },
+    {224,16 }, {201,24 }, {181,16 }, {166,16 }, {152,16 }, {137,16 }, {123,16 }, {108,16 }, { 94,16 }, { 79,16 }, { 65,16 }, { 51,16 }, { 36,16 }, { 22,16 }, {  4,16 },
+    {  4,32 }, { 25,32 }, { 40,32 }, { 54,32 }, { 69,32 }, { 83,32 }, { 98,32 }, {112,32 }, {126,32 }, {141,32 }, {155,32 }, {170,32 }, {184,32 }, {224,32 },
+    {202,48 }, {182,48 }, {163,48 }, {148,48 }, {134,48 }, {119,48 }, {105,48 }, { 90,48 }, { 76,48 }, { 61,48 }, { 47,48 }, { 33,48 }, { 18,48 }, {  2,48 },
+    {  2,64 }, { 20,64 }, { 38,64 }, { 92,64 }, {146,64 }, {188,64 }, {202,64 }, {217,64 }, {224,64 }
+}, {
+    // LED Index to Flags
+    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,
+    1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,
+    1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,
+    1, 1, 1, 4, 1, 1, 1, 1, 1
+}};
