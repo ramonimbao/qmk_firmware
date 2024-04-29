5fd10242887f0bc92f51adaa7576aa001e590b2c
Ramon Imbao <ramonimbao@gmail.com>
2024-04-29 13:53:56 +0800

    Add replicant86

---
+++ keyboards/replicant86/config.h
@@ -0,0 +1,16 @@
+// Copyright 2023 Ramon Imbao (@ramonimbao)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// I2C OLED defines
+#define I2C1_SCL_PIN B8
+#define I2C1_SDA_PIN B9
+
+#define I2C1_SCL_PAL_MODE 1
+#define I2C1_SDA_PAL_MODE 1
+
+#define I2C1_TIMINGR_SCLDEL 3U
+#define I2C1_TIMINGR_SDADEL 1U
+#define I2C1_TIMINGR_SCLH 	3U
+#define I2C1_TIMINGR_SCLL   9U
