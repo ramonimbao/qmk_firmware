fe31a232f904971b2d4c9deef2611e5716d9935f
Ramon Imbao <ramonimbao@gmail.com>
2025-02-20 16:29:48 +0800

    Add the '75LHNP'

---
+++ keyboards/rdg/lhnp75/config.h
@@ -0,0 +1,17 @@
+// Copyright 2025 Ramon Imbao (@ramonimbao)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* VIA custom menu*/
+#define VIA_EEPROM_CUSTOM_CONFIG_SIZE 2
+
+/* RGB */
+#define WS2812_SPI_DRIVER SPID2
+#define WS2812_SPI_MOSI_PAL_MODE 0
+#define WS2812_SPI_SCK_PIN B13
+#define WS2812_SPI_SCK_PAL_MODE 0
+#define WS2812_EXTERNAL_PULLUP
+#define RGB_MATRIX_LED_COUNT 108
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
